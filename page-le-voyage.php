d54c3bf9bfb3137777206558a32586814569a836
Akamis <mrtetex@gmail.com>
2016-12-15 22:27:34 +0100

    Add code for the map

--- page-le-voyage.php
+++ page-le-voyage.php
@@ -11,32 +11,125 @@
     <script type="text/javascript">
 
 function initMap() {
-  var directionsService = new google.maps.DirectionsService;
-  var directionsDisplay = new google.maps.DirectionsRenderer;
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 7,
-    center: {lat: 45.85, lng: 4.65}
-  });
-  directionsDisplay.setMap(map);
-  calculateAndDisplayRoute(directionsService, directionsDisplay);
-}
+							//Cordonné
+						  var myLatLng_sso = {lat: 45.638927, lng: 4.867375};
+						  var myLatLng_valence = {lat: 44.933869, lng: 4.888669};
 
-var myLatLng = new google.maps.LatLng(45.765136,4.835464);
-var myLatLng2 = new google.maps.LatLng(44.933869,4.888669);
+						  //Création de la map
+						  var map = new google.maps.Map(document.getElementById('map'), {
+						    zoom: 8,
+						    center: myLatLng_sso
+						  });
 
-function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-  directionsService.route({
-    origin: myLatLng,
-    destination: myLatLng2,
-    travelMode: google.maps.TravelMode.DRIVING
-  }, function(response, status) {
-    if (status === google.maps.DirectionsStatus.OK) {
-      directionsDisplay.setDirections(response);
-    } else {
-      window.alert('Directions request failed due to ' + status);
-    }
-  });
+						  //Marker avec animation
+						  var marker = new google.maps.Marker({
+						    position: myLatLng_valence,
+						    map: map,
+						    title: 'Valence',
+						    animation : google.maps.Animation.BOUNCE
+						  });
+
+						  	var icone = 'icon.png';
+						  	//Autre marker
+						    var marker1 = new google.maps.Marker({
+						    position: myLatLng_sso,
+						    map: map,
+						    animation : google.maps.Animation.DROP,
+						    title: 'Maison !'
+						  });
+
+						  //Affichage d'une fenettre d'info quand on clique sur le marker
+						  var info_content = '<h1>Valence</h1>'+
+						  '<p>C\'est la que je vais ! </p>';
+
+						  var infowindow = new google.maps.InfoWindow({
+						   		content : info_content,
+						   		maxWidth: 300
+						   });
+
+						   marker.addListener('click', function() {
+						    infowindow.open(map, marker);
+						    marker.setAnimation(null);
+						  });
+
+						  //Polyline
+						  var path_chemin = [
+						  {lat: 45.765136, lng: 4.835464},
+						  {lat: 44.933869, lng: 4.888669}
+						  ];
+						  var chemin = new google.maps.Polyline({
+						  	path : path_chemin,
+						  	geodesic: true,
+						  	editable : true,
+						  	strokeColor : '#FF0000',
+						  	strokeOpacity : 1
+						  })
+
+						  chemin.setMap(map);
+
+
+						}
+
+//<![CDATA[
+var map = null;
+function initialize() {
+  var myOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(43.907787,-79.359741),
+    mapTypeControl: true,
+    mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU},
+    navigationControl: true,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  map = new google.maps.Map(document.getElementById("map_canvas"),
+                                myOptions);
+ 
+  google.maps.event.addListener(map, 'click', function() {
+        infowindow.close();
+        });
+
+  // Add markers to the map
+  // Set up three markers with info windows 
+    
+      var point = new google.maps.LatLng(43.65654,-79.90138); 
+      var marker = createMarker(point,'<div style="width:240px">Some stuff to display in the First Info Window. With a <a href="http://www.econym.demon.co.uk">Link<\/a> to Mike Williams&apos; home page<\/div>')
+ 
+      var point = new google.maps.LatLng(43.91892,-78.89231);
+      var marker = createMarker(point,'Some stuff to display in the<br>Second Info Window')
+ 
+      var point = new google.maps.LatLng(43.82589,-79.10040);
+      var marker = createMarker(point,'Some stuff to display in the<br>Third Info Window')
+
 }
+ 
+var infowindow = new google.maps.InfoWindow(
+  { 
+    size: new google.maps.Size(150,50)
+  });
+    
+function createMarker(latlng, html) {
+    var contentString = html;
+    var marker = new google.maps.Marker({
+        position: latlng,
+        map: map,
+        zIndex: Math.round(latlng.lat()*-100000)<<5
+        });
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(contentString); 
+        infowindow.open(map,marker);
+        });
+}
+ 
+
+    // This Javascript is based on code provided by the
+    // Community Church Javascript Team
+    // http://www.bisphamchurch.org.uk/   
+    // http://econym.org.uk/gmap/
+    // from the v2 tutorial page at:
+    // http://econym.org.uk/gmap/basic1.htm 
+//]]>
+
 
     </script>
     <script async defer
@@ -44,3 +137,7 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
     </script>
   </body>
 </html>
+
+
+
+
--- page-test.php
+++ page-test.php
@@ -21,71 +21,77 @@ get_header(); ?>
 				<div id="primary" class="col-md-9 content-area">
 					<main id="main" class="site-main" role="main">
 						 <body>
-						 	<h2>Le trajet</h2>
-						 		<ol>
-						 			<li>Test</li>
-						 		</ol>
-						    <div id="map"></div>
-						    <script type="text/javascript">
+					 		<?php //The Loop 
+					 		while ( have_posts() ) : the_post(); 
+								 the_content();
+							endwhile; // End of the loop. ?>
+
+					    <div id="map"></div>
+
+					   <script type="text/javascript">
+
+						//Cordonné ['Name', lat, lng]
+						var pos_markers = [
+							['Saint Symphorien D\'ozon', 45.638927, 4.867375],
+							['Tournon sur Rhone', 45.031149, 4.776435],
+							['Valence', 44.933869, 4.888669],
+							['Longo-Mai', 43.977848, 5.744968],
+						];
+
+						var pos_info =
+							'<div id="content">'+
+						      '<div id="siteNotice">'+
+						      '</div>'+
+						      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+						      '<div id="bodyContent">'+
+						      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+						      'sandstone rock formation in the southern part of the '+
+						      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+						      'south west of the nearest large town, Alice Springs; 450&#160;km '+
+						      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+						      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+						      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+						      'Aboriginal people of the area. It has many springs, waterholes, '+
+						      'rock caves and ancient paintings. Uluru is listed as a World '+
+						      'Heritage Site.</p>'+
+						      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+						      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+						      '(last visited June 22, 2009).</p>'+
+						      '</div>'+
+						      '</div>';
+
 
 						function initMap() {
-							//Cordonné
-						  var myLatLng_home = {lat: 45.765136, lng: 4.835464};
-						  var myLatLng_valence = {lat: 44.933869, lng: 4.888669};
-
 						  //Création de la map
 						  var map = new google.maps.Map(document.getElementById('map'), {
 						    zoom: 8,
-						    center: myLatLng_home
-						  });
+						    center: {lat: 45.638927, lng: 4.867375}
+						    });
+						  setMarkers(map, pos_info);
+						}
 
-						  //Marker avec animation
-						  var marker = new google.maps.Marker({
-						    position: myLatLng_valence,
-						    map: map,
-						    title: 'Valence',
-						    animation : google.maps.Animation.BOUNCE
-						  });
+						  function setMarkers(map, html) {
+							  for (var i = 0; i < pos_markers.length; i++) {
+							    var markers = pos_markers[i];
+							    var marker = [];
+							    marker[i] = new google.maps.Marker({
+							      position: {lat: markers[1], lng: markers[2]},
+							      map: map,
+							      title: markers[0]
+							    });
+							  	
+							  	var contentString = html;
+							  	var infowindow = new google.maps.InfoWindow({
+							  		
+							  	});
 
-						  	var icone = 'icon.png';
-						  	//Autre marker
-						    var marker1 = new google.maps.Marker({
-						    position: myLatLng_home,
-						    map: map,
-						    animation : google.maps.Animation.DROP,
-						    title: 'Maison !'
-						  });
+			  				 	marker[i].addListener('click', function() {
+			  				 		infowindow.setContent(contentString);
+							 		infowindow.open(map, marker[i]);
+							  	});
 
-						  //Affichage d'une fenettre d'info quand on clique sur le marker
-						  var info_content = '<h1>Valence</h1>'+
-						  '<p>C\'est la que je vais ! </p>';
-
-						  var infowindow = new google.maps.InfoWindow({
-						   		content : info_content,
-						   		maxWidth: 300
-						   });
-
-						   marker.addListener('click', function() {
-						    infowindow.open(map, marker);
-						    marker.setAnimation(null);
-						  });
-
-						  //Polyline
-						  var path_chemin = [
-						  {lat: 45.765136, lng: 4.835464},
-						  {lat: 44.933869, lng: 4.888669}
-						  ];
-						  var chemin = new google.maps.Polyline({
-						  	path : path_chemin,
-						  	geodesic: true,
-						  	editable : true,
-						  	strokeColor : '#FF0000',
-						  	strokeOpacity : 1
-						  })
-
-						  chemin.setMap(map);
-
-
+			  					
+							}
 						}
 
 						    </script>
